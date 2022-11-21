c0f3d5d0e4c1d95244740ebffc3d5c68246e7d0c
Alex <babel.softwaresolutions-freelance@digitalcareerinstitute.org>
2022-11-21 11:35:51 +0100

    Added URL.createObjectURL method

--- file_upload/js/script.js
+++ file_upload/js/script.js
@@ -14,6 +14,7 @@ avatarInput.addEventListener('change', evt => {
     // Fuege dem FileReader einen EventListener (load) hinzu
     // das load-Event wird gefeuert, wenn der FileReader die Datei erfolgreich eingelesen hat
     reader.addEventListener("load", (evt) => {
+        console.log(evt);
         // Konvertiere die eingelesene Datei mittels des FileReaders
         // in einen Base64-kodierten String
         // Und hinterlege diesen Base64 String als image.src des Profilbilds
@@ -28,4 +29,17 @@ avatarInput.addEventListener('change', evt => {
         // Lese mittels FileReader Datei ein
         reader.readAsDataURL(file);
     }
+
+    // Weitere Moeglichtkeit in synchroner Weise statt dem FileReader
+    // URL.createObjectURL wird synchron ausgefuehrt, also Code wartet auf dessen Ausfuehrung
+    // und die Datei wird im Arbeitsspeicher gespeichert, also nicht als Base64 String
+    // avatarImg.src = URL.createObjectURL(file);
+
+    // Um Bild wieder aus dem Arbeitsspeicher zu nehmen, wird URL.revokeObjectURL genutzt
+    // URL.revokeObjectURL(file);
+
+    // Lasse img wieder anzeigen
+    // avatarImg.hidden = false;
 });
+
+
